c7160772835469395e92776bd551d0e9b7642e51
17mcpc14 <36224815+17mcpc14@users.noreply.github.com>
2018-06-20 18:12:11 +0530

    Fixed large dimension/memory issue with insertion of column,row for new user,movie

--- i4step/ipremf.py
+++ i4step/ipremf.py
@@ -5,7 +5,7 @@ import datetime
 
 quarter_map = {}
 
-for my_file in sorted(glob.glob("./monthly-data/*")):
+for my_file in sorted(glob.glob("../monthly-data/*")):
     if (basename(my_file) != "1999-11" and basename(my_file) != '1999-12' and basename(my_file) != '2006-1'):
         dt = datetime.datetime.strptime(basename(my_file), '%Y-%m') + datetime.timedelta(days=1)
 
@@ -22,6 +22,9 @@ def build_matrix(f):
     f.close()
 
     global P
+    
+    print("shape of P:",p.shape)
+
     for line in lines :
         line = line.rstrip()
 
@@ -29,28 +32,23 @@ def build_matrix(f):
         movie_id = int(individual_rating[0])
         user_id = int(individual_rating[1])
         if(user_id not in users):
+            ux =  bisect_left( sorted(users), user_id, 0 , len(users))
             users.append(user_id)
+            P = np.insert(P, ux, 0, axis=1)      
         if(movie_id not in movies):
+            ix =  bisect_left( sorted(movies), movie_id, 0 , len(movies))
             movies.append(movie_id)
-
-    x = np.amax(movies) - P.shape[0] 
-    P = np.vstack((P, np.zeros((x,P.shape[1]))))
-    y = np.amax(users) - P.shape[1] 
-    P = np.hstack((P,np.zeros((P.shape[0], y))))
-
-    for line in lines :
-        line = line.rstrip()
-
-        individual_rating = line.split(",")
-        movie_id = int(individual_rating[0])
-        user_id = int(individual_rating[1])
-        rating = int(individual_rating[2])
-
+	    P = np.insert(P, ix, 0, axis=0)   
+        
         P[movies.index(movie_id), users.index(user_id)] =  rating
+
+    print("shape of P:",p.shape)
+
+    return P
 
 for i in sorted(quarter_map.keys()):
     print(i)
     build_matrix(open(quarter_map[i]))
     
-    np.savetxt('monthly-mat2/'+i, P, fmt='%.3f')
+    np.savetxt('../monthly-mat2/'+i, P, fmt='%.3f')
 
